71bc37407b25e1ccfc7df13a19eb379b5cfda202
nuclear <nuclear@fedora.linx>
2023-11-30 14:01:30 -0500

    Feature: Add authentication requirement for update, delete and add incident. Add register and login options. Make MongoDB database to store login credentials.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // Define connection url, ports etc
 const app = express();
@@ -16,6 +18,11 @@ app.use(cors({
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Expose-Headers', 'Authorization');
+  next();
+});
 
 // Connect to MongoDB
 mongoose.connect(uri)
@@ -40,18 +47,145 @@ const incidentSchema = new mongoose.Schema(
     closedAt: Date
 }
 );
-console.log(" - Schema defined")
+console.log(" - Incident schema defined")
+
+// Define username-password schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+console.log(" - Username-Password schema defined")
 
 
 const Incident = mongoose.model('incident', incidentSchema);
+const User = mongoose.model('User', userSchema);
 
 app.use(express.json());
 app.use(express.static('public'));
+
+// Auth routes
+// Registration
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const user = new User({ username, password: hashedPassword });
+
+    // Save the user to the database
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+//Login
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find the user by username
+    const user = await User.findOne({ username });
+
+    // Check if the user exists and the password is correct
+    if (user && await bcrypt.compare(password, user.password)) {
+      // Generate a JWT token
+      const token = jwt.sign({ userId: user._id }, 'secret-key', { expiresIn: '1h' });
+
+      // Send the token to the client
+      res.json({ token });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// Protected routes
+// function authenticateToken(req, res, next) {
+//   console.log(" - In authentication");
+//   const token = req.header('Authorization');
+
+//   if (!token) {
+//     console.log(" - No token") 
+//     return res.status(401).json({ message: 'Access denied' });
+// }
+
+//   console.log(' - Token received:', token);
+
+//   jwt.verify(token, 'secret-key', (err, user) => {
+//     if (err) {
+//       console.log (" - Invalid token");
+//       const decoded = jwt.decode(token, { complete: true });
+//       console.log(decoded);
+//       console.error('JWT Verification Error:', err);
+//       return res.status(403).json({ message: 'Invalid token' });
+//     }
+
+//     req.user = user;
+//     next();
+//   });
+// }
+
+// Protected routes
+function authenticateToken(req, res, next) {
+  console.log(" - In authentication");
+  const authHeader = req.header('Authorization');
+
+  if (!authHeader) {
+    console.log(" - No Authorization header");
+    return res.status(401).json({ message: 'Access denied' });
+  }
+
+  const [bearer, token] = authHeader.split(' ');
+
+  if (bearer !== 'Bearer' || !token) {
+    console.log(" - Invalid Authorization header format");
+    return res.status(401).json({ message: 'Invalid Authorization header format' });
+  }
+
+  console.log(' - Token received:', token);
+
+  jwt.verify(token, 'secret-key', (err, user) => {
+    if (err) {
+      console.log(" - Invalid token");
+      const decoded = jwt.decode(token, { complete: true });
+      console.log(decoded);
+      console.error('JWT Verification Error:', err);
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+
+app.get('/protected', authenticateToken, (req, res) => {
+  res.json({ message: 'This is a protected route', user: req.user });
+});
+
 
 // Define routes
 app.get('/', (req, res) => {
   console.log('Route received');
   res.render('landing_page', { title: 'Landing Page' });
+});
+
+app.get('/login.html', (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+app.get('/register.html', (req, res) => {
+  res.render('register', { title: 'register' });
 });
 
 app.get('/landing_page.html', (req, res) => {
@@ -71,9 +205,17 @@ app.get('/update_incident.html', (req, res) => {
   res.render('update_incident', { title: 'Update Incident' });
 });
 
+app.get('/login.html', (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+app.get('/register.html', (req, res) => {
+  res.render('register', { title: 'Register' });
+});
+
 // Define methods
 // Add incident
-app.post('/incidents', async (req, res) => {
+app.post('/incidents', authenticateToken, async (req, res) => {
   try {
     // Set default values for created at and actions taken
     req.body.createdAt = new Date();
@@ -134,7 +276,7 @@ app.get('/incidents/all', async (req, res) => {
 });
 
 // Update incident
-app.put('/incidents/:incidentId', async (req, res) => {
+app.put('/incidents/:incidentId', authenticateToken, async (req, res) => {
   try {
     // Retrieve incidentId from URL params
     const incidentId = req.params.incidentId;
